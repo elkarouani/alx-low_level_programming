d39075c025637b6967036d1424f38b5c5fef512b
elkarouani <elkarouani@gmail.com>
2023-03-16 06:07:28 -0700

    Task: Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int main() {
-	puts("Programming is like building a multilingual puzzle\n");
+	char Mystr[] = "Programming is like building a multilingual puzzle\n";
+	puts(Mystr);
 	return 0;
 }
