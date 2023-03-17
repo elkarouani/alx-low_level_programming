8bbb1c2ac5bd53efb0b1d43b632c3f860b45e27e
elkarouani <elkarouani@gmail.com>
2023-03-17 05:12:43 -0700

    Task: Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,14 @@ int main(void)
 		if (number < 10)
 			putchar('0' + number);
 		if (number >= 10 && number % 10 != 0 && number % 100 < number % 10)
-			putchar(48 + number);
-		if (number != 99 && number % 10 != 0 && number % 100 < number % 10)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(48 + number);
+			
+			if (number != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
