a7c974e99f58249c66ee013e81480bdedbe03e27
elkarouani <elkarouani@gmail.com>
2023-03-17 04:19:38 -0700

    Task: Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercas
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	int number;
+	char character;
+
+	for (number = 0; number < 10; number++)
+		putchar(48 + number);
+	for (character = 'a'; character <= 'f'; character++)
+		putchar(charachter);
+	putchar('\n');
+	return (0);
+}
