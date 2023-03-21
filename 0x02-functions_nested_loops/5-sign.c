754a128459406a9745f6c0c25cdc50125f8915fb
elkarouani <elkarouani@gmail.com>
2023-03-21 05:49:24 -0700

    Task: Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign - prints the sign of a number.
  * @n: the number to check
- * Return: 
+ * Return:
  * > 1 and prints + if n is greater than zero,
- * > 0 and prints 0 if n is zero, 
+ * > 0 and prints 0 if n is zero,
  * > -1 and prints - if n is less than zero
  */
 int print_sign(int n)
@@ -13,17 +13,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		return (1);
 	}
 	if (n == 0)
 	{
 		_putchar('0');
-		return 0;
+		return (0);
 	}
 	if (n < 0)
 	{
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
-	return 0;
+	return (0);
 }
