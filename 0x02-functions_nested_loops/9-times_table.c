ab7f685755778317fd258ce91b0d27df48792c6d
elkarouani <elkarouani@gmail.com>
2023-03-21 06:43:58 -0700

    Task: Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ * Return: null
+ */
+void times_table(void)
+{
+	int multiplicand;
+	int multiplier;
+
+	for (multiplier = 0; multiplier <= 9; multiplier++)
+	{
+		for (multiplicand = 0; multiplicand <= 9; multiplican++)
+		{
+			int product = multiplicand * multiplier;
+
+			if (multiplicand == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(multiplicand * multiplier + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (product >= 10)
+			{
+				_putchar((product / 10) % 10 + 48);
+				_putchar(product % 10 + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
