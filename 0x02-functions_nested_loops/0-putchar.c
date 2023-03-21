2a20771bf6c74977b344c86e4ba5ab3638c913ad
elkarouani <elkarouani@gmail.com>
2023-03-21 02:20:34 -0700

    Task: _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
