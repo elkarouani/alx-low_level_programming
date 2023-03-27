8fa7d0b1c60b24a1b4c46e8be5d7338da3a663fd
elkarouani <elkarouani@gmail.com>
2023-03-27 08:47:11 -0700

    Task: add 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		len++;
 	
 	halflen = (len % 2) == 0 ? len / 2 : (len - 1) / 2;
-	for (i = (len - halflen); i <= len; i++)
+	for (i = (len - halflen); i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
