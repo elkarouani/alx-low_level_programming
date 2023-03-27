4bb123569bc6830c694eef39d6ae408ad4543baa
elkarouani <elkarouani@gmail.com>
2023-03-27 06:56:41 -0700

    Task: 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - updates the integer param's value it points to to 98.
- * @*n: the requested int pointer
+ * @n: the requested int pointer
  * Return: Null
  */
 void reset_to_98(int *n)
