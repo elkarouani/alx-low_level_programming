8571caa80b2021a878d02918ab301f46ab498b80
elkarouani <elkarouani@gmail.com>
2023-03-27 08:36:37 -0700

    Task: add 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 -  prints every other character of a string, 
+ * puts2 -  prints every other character of a string,
  * starting with the first character
  * @str: the characters array to print
  * Return: Null
