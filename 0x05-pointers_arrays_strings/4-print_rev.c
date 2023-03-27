9d92d3e2b4e13139af35830365a60f88033ab47b
elkarouani <elkarouani@gmail.com>
2023-03-27 08:01:04 -0700

    Task: update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,23 +7,12 @@
  */
 void print_rev(char *s)
 {
-	char tmp;
-	int i, len, len1;
+	int i = 0;
+	int len = 0;
 
-	len = 0;
-	len1 = 0;
-
-	while (s[len] != '\0')
-	{
+	while (s[i] != '\0')
 		len++;
-	}
-
-	len1 = len - 1;
-
-	for (i = 0; i < len / 2; i++)
-	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
-	}
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
