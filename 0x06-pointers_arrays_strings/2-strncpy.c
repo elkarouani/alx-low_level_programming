413e4d6697e33603a2d38adc5a9ea9af92ed3038
elkarouani <elkarouani@gmail.com>
2023-03-30 03:46:43 -0700

    Task: strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	
 	while (i < n && src[i] != '\0')
-		dest[i++] = src[i++];
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	while (i < n)
-		dest[i++] = '\0';
-	return dest;
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
