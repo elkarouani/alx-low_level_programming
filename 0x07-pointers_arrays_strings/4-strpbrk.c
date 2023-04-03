5c9b5eb67e9287985fc987f1e09db6c7a42a54f3
elkarouani <elkarouani@gmail.com>
2023-04-03 06:56:28 -0700

    Task: strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  * *_strpbrk - searches a string for any of a set of bytes
  * @s: string to lookup
  * @accept: bytes to search with it
- * Return: a pointer to the byte in s that matches one of 
+ * Return: a pointer to the byte in s that matches one of
  * the bytes in accept, or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
