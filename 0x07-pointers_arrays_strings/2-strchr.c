0446e8ccb16be1ad8b35c63e8b4996c20f8e4339
elkarouani <elkarouani@gmail.com>
2023-04-03 05:14:59 -0700

    Task: strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * *_strchr - locates a character in a string
  * @s: String to be lookup
  * @c: the character to locate
- * Return: a pointer to the first occurrence of c in the string s, 
+ * Return: a pointer to the first occurrence of c in the string s,
  * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
@@ -15,13 +15,10 @@ char *_strchr(char *s, char c)
 	while (1)
 	{
 		a = *s++;
+
 		if (a == c)
-		{
 			return (s - 1);
-		}
 		if (a == 0)
-		{
 			return (NULL);
-		}
 	}
 }
