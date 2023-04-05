7ca5ef1a5ce307b12432510d3f69e4cf2d5b0682
elkarouani <elkarouani@gmail.com>
2023-04-05 15:17:21 -0700

    Task: adding 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: Number to be calculated
+ * Return: factorial of the requested number
+ */
+int factorial(int n)
+{
+	return (n == 0 ? 1 : n * factorial(n - 1));
+}
