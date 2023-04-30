7adfbbf00554be4216f2aeddcf38ca76f65a81c5
elkarouani <elkarouani@gmail.com>
2023-04-29 17:52:07 -0700

    Task: 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (temporary_node && i < index)
 	{
-		temporary_node = head->next;
+		temporary_node = temporary_node->next;
 		i++;
 	}
 
