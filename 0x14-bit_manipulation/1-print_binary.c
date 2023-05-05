2b6b52ebb15a666f39c42a983a93e5fc88cd671b
elkarouani <elkarouani@gmail.com>
2023-05-05 16:27:39 -0700

    Task: 0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++);
+
+	for (; shifts >= 0; shifts--)
+		printf(((n >> shifts) & 1) ? "1" : "0");
+}
