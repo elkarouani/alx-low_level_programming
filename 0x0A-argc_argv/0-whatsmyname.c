4ccfc090004f40ec9ceee2ed3b9a208e7eab99ee
elkarouani <elkarouani@gmail.com>
2023-06-09 02:34:16 -0700

    Task: 0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,16 +7,28 @@
  *
  * Return: 0 (exit with success)
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	char *program_name = *argv;
 
 	while (*program_name != '\0')
 	{
 		_putchar(*program_name);
-		*program_name += 1;
+		program_name += 1;
 	}
 
 	_putchar('\n');
 	return (0);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+
 int _putchar(char c);
 int _atoi(char *s);
 
