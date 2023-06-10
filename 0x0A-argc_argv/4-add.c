69b00297e4ff9d81e74133c94990fd487758894b
elkarouani <elkarouani@gmail.com>
2023-06-10 15:42:19 -0700

    Task: 0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main program that adds positive numbers
+ * @argc: number of arguments
+ * @argv: the arguments passed by the program
+ *
+ * Return: 0 (exit with success) or 1 (exit with error)
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int total = 0;
+
+	while (i <= argc - 2)
+	{
+		if (!_atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		result += _atoi(argv[i]);
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string to be converted
+ * Return: the integer parsed from the requested string
+ */
+int _atoi(char *s)
+{
+	int len = 0;
+	int sign = 1;
+	int isNumberDetected = 0;
+	int result = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[len] == '-')
+			sign *= -1;
+		if (s[len] == '+')
+			sign *= +1;
+		if (s[len] >= '0' && s[len] <= '9')
+		{
+			isNumberDetected = 1;
+			if (result > 0)
+				result *= 10;
+			if (result == 2147483640 && s[len] == '8')
+				result = -2147483648;
+			else
+				result += s[len] - 48;
+		}
+		if ((s[len] < '0' || s[len] > '9') && isNumberDetected == 1)
+			break;
+		len++;
+	}
+
+	return (result == -2147483648 ? result : sign * result);
+}
