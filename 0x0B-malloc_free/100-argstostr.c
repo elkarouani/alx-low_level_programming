2030354baad71c286e8da7de692648bc6eeb8d03
elkarouani <elkarouani@gmail.com>
2023-06-13 17:03:01 -0700

    Task: 0x0B-malloc_free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,62 +11,40 @@
 char *argstostr(int ac, char **av)
 {
 	char *new_string;
-	/*
-	int i = 1;
-	*/
+	int total_length = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
 
 	if (ac <= 0 || av == NULL)
 		return (NULL);
 
-	new_string = *(av);
-	/*
 	while (i < ac)
 	{
-		new_string = str_concat(new_string, *(av + i++));
-		if (new_string == NULL)
-			return (NULL);
+		while (*(*(av + i) + j) != '\0')
+		{
+			total_length++;
+			j++;
+		}
+
+		total_length++;
+		i++;
+		j = 0;
 	}
-	*/
 
-	return (new_string);
-}
-
-/**
- * str_concat - concatenates two strings.
- * @s1: string to concatinate
- * @s2: string to concatinate with
- *
- * Return: pointer of the concatinated string
- */
-char *str_concat(char *s1, char *s2)
-{
-	char *concat_string;
-	int len1 = 0;
-	int len2 = 0;
-	int i = 0;
-	int j = 0;
-
-	while (s1 && *(s1 + len1) != '\0')
-		len1++;
-	while (s2 && *(s2 + len2) != '\0')
-		len2++;
-
-	concat_string = (char *) malloc(sizeof(char) * (len1 + len2) + 1);
-	if (concat_string == NULL)
+	new_string = (char *) malloc(sizeof(char) * total_length);
+	if (new_string == NULL)
 		return (NULL);
 
-	while (i < len1)
+	i = 0;
+	while (i < ac)
 	{
-		*(concat_string + i) = *(s1 + i);
-		i++;
+		while (*(*(av + i) + j) != '\0')
+			*(new_string + k++) = *(*(av + i) + j++);
+		if (i < ac - 1)
+			*(new_string + k) = '\n';
+		j = 0;
 	}
 
-	while (j < len2)
-	{
-		*(concat_string + i) = *(s2 + j);
-		i++;
-		j++;
-	}
-
-	return (concat_string);
+	return (new_string);
 }
