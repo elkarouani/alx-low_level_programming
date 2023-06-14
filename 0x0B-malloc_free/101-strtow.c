41dd6a366bfffc84b2691561c5d91f146838d2fb
elkarouani <elkarouani@gmail.com>
2023-06-14 09:04:05 -0700

    Task: 0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,47 +2,36 @@
 #include "main.h"
 
 /**
- * count_words - counts the number of words in a string
- * @s: string to lookup
+ * strtow - splits a string into words.
+ * @str: the string to be splited
  *
- * Return: number of words
- */
-int count_words(char *s)
-{
-	int capt_separator = 0;
-	int words_count = 0;
-	int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-		if (*(s + i) == ' ')
-			capt_separator = 0;
-		else if (capt_separator == 0)
-		{
-			capt_separator = 1;
-			words_count++;
-		}
-	}
-
-	return (words_count);
-}
-
-/**
- * **strtow - splits a string into words
- * @str: string to split
- *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
+ * Return: pointer of splitted words array, or NULL if it fails
  */
 char **strtow(char *str)
 {
 	char **splited_words;
-	int k = 0, len = 0, words_count;
+	char *current_word;
+	char delimiter = ' ';
+	int detect_delimiter = 0;
+	int words_count = 0;
+	int len = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
 
-	while (*(str + len))
-		len++;
+	if (str == NULL)
+		return (NULL);
 
-	words_count = count_words(str);
+	while (*(str + len) != '\0')
+	{
+		if (*(str + len++) == delimiter)
+			detect_delimiter = 1;
+		else if (detect_delimiter == 1)
+		{
+			detect_delimiter = 0;
+			words_count++;
+		}
+	}
 	if (words_count == 0)
 		return (NULL);
 
@@ -50,7 +39,29 @@ char **strtow(char *str)
 	if (splited_words == NULL)
 		return (NULL);
 
-	*(splited_words + k) = NULL;
+	while (i < words_count)
+	{
+		while (j < len)
+		{
+			if (*(str + j) != delimiter)
+				*(current_word + k++) = *(str + j++);
+			else
+				break;
+		}
+		if (k == 0)
+			continue;
+		
+		*(splited_words + i) = (char *) malloc(sizeof(char) * k + 1);
+		if (*(splited_words + i) == NULL)
+			return (NULL);
+			
+		*(current_word + k++) = '\0';
+		*(splited_words + i++) = current_word;
 
+		k = 0;
+		current_word = NULL;
+	}
+
+	*(splited_words + words_count + 1) = NULL;
 	return (splited_words);
 }
