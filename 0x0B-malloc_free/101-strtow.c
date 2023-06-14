551f767c646030bfe01e0bd67306a0706251f58f
elkarouani <elkarouani@gmail.com>
2023-06-13 17:45:37 -0700

    Task: 0x0B-malloc_free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * count_words - counts the number of words in a string
+ * @s: string to lookup
+ *
+ * Return: number of words
+ */
+int count_word(char *s)
+{
+	int capt_separator = 0;
+	int words_count = 0;
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == ' ')
+			capt_separator = 1;
+		else if (capt_separator == 1)
+		{
+			capt_separator = 0;
+			words_count++;
+		}
+	}
+
+	return (words_count);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **splited_words;
+	char *tmp;
+	int len = 0;
+	int words_count = 0;
+	int i, k = 0, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+
+	words_count = count_word(str);
+	if (words_count == 0)
+		return (NULL);
+
+	splited_words = (char **) malloc(sizeof(char *) * (words_count + 1));
+	if (splited_words == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (*(str + i) == ' ' || *(str + i) == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				
+				while (start < end)
+					*tmp++ = *(str + start++);
+				
+				*tmp = '\0';
+				*(splited_words + k) = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	*(splited_words + k) = NULL;
+
+	return (splited_words);
+}
