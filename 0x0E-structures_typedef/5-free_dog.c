7912b7ab75ba030a29cb3798a4d4101af714c6fd
elkarouani <elkarouani@gmail.com>
2023-06-20 03:32:19 -0700

    Task: 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs.
+ * @d: pointer to the dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
