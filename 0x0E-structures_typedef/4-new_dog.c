6433ca778d425f85a71ceb525f3e1b5ffc5ddd45
elkarouani <elkarouani@gmail.com>
2023-06-20 03:28:03 -0700

    Task: 0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int name_len = 0, owner_len = 0, i = 0, j = 0;
-	
+
 	while (*(name + name_len) != '\0')
 		name_len++;
 	while (*(owner + owner_len) != '\0')
@@ -20,7 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	
+
 	dog->name = malloc(sizeof(char) * (name_len + 1));
 	if (dog->name == NULL)
 	{
