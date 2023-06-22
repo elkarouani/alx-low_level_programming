c291ee73af3a3322c367b21c7faaa8a6661f7801
elkarouani <elkarouani@gmail.com>
2023-06-22 01:59:25 -0700

    Task: 0x0F-function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,12 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	for (i = 0; operations[i].op; i++)
+	while (operations[i].op)
 	{
 		if (strcmp(operations[i].op, s) == 0)
 			return (operations[i].f);
+		i++;
 	}
 
 	return (NULL);
