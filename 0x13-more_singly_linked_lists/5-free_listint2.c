4ad8593dcd5713dbadae6eb093bc8c275c84b68d
elkarouani <elkarouani@gmail.com>
2023-07-05 13:56:39 -0700

    Task: 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: the first node of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temp;
+
+	if (head)
+	{
+		current = *head;
+
+		while ((temp = current))
+		{
+			current = current->next;
+			free(temp);
+		}
+		
+		*head == NULL;
+	}
+}
