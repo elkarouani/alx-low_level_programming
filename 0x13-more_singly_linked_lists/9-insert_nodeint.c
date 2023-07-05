0d32feec86f7f1031390132c7b05321cf52bcedd
elkarouani <elkarouani@gmail.com>
2023-07-05 14:55:45 -0700

    Task: 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp = *head;
 	unsigned int i = 0;
 
-	if (!new_node)
+	if (!new_node || !head)
 		return (NULL);
 
 	if (idx == 0)
