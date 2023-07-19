cdc8937c5ab5c90f12a77dc3279484fff241da88
elkarouani <elkarouani@gmail.com>
2023-07-19 09:19:56 -0700

    Task: 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: the number to lookup
+ * @index: the index of the degit
+ *
+ * Returns: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bit;
+
+	if (index > 64)
+		return (-1);
+
+	bit = n >> hold;
+	return (bit & 1);
+}
