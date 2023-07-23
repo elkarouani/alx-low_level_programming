262f45de9ff61295d4e7bd28af46e9e4d050dadd
elkarouani <elkarouani@gmail.com>
2023-07-23 09:39:54 -0700

    Task: 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -71,7 +71,7 @@ int main(int argc, char **argv)
 		w_bits = write(to_desc, buffer, r_bits);
 		if (to_desc == -1 || w_bits == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", *(argv + 2));
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", *(argv + 2));
 			exit(99);
 		}
 
